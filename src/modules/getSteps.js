51c313822bb32507287e5e3813df3624efe3dd66
Irina864 <irina.peslyak.1864@gmail.com>
2025-03-17 01:07:39 +0300

    fix fetch

--- src/modules/getSteps.js
+++ src/modules/getSteps.js
@@ -2,7 +2,7 @@ import { createStepItem } from './createStepItem';
 
 // getting and rendering steps
 export function getProcess() {
-  fetch('../data/processData.json')
+  fetch('./data/processData.json')
     .then((response) => {
       if (!response.ok) {
         throw new Error('Network response was not ok ' + response.statusText);
